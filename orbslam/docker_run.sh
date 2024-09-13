0088caf993b73daccce250216b72a64e5a99a38c
teju81 <teju81@gmail.com>
2024-09-13 12:00:05 +0530

    Update docker_run.sh

--- orbslam/docker_run.sh
+++ orbslam/docker_run.sh
@@ -2,6 +2,9 @@
 # When <container_name> is not provided default container_name ros2_container is used
 container_name=${1:-orbslam_container}
 image_name=${2:-orbslam_docker}
+
+xhost +local:root
+
 docker run --rm -it --privileged --network=host --ipc=host -v "/dev:/dev" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/raviteja/code/datasets/:/root/code/datasets/ --name $container_name $image_name bash
 # docker run -it --rm --volume="/home/raviteja/code/MonoGS/:/MonoGS/" --env="NVIDIA_VISIBLE_DEVICES=all" --env="NVIDIA_DRIVER_CAPABILITIES=all" \
 #     --net=host \
@@ -13,3 +16,5 @@ docker run --rm -it --privileged --network=host --ipc=host -v "/dev:/dev" -e DIS
 #     --gpus all \
 #     --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
 #     --name $container_name $image_name bash
+
+xhost -local:root
