67e4c8217df1fd55dca28f1d67048b74483f356c
teju81 <teju81@gmail.com>
2024-12-19 20:46:47 +0530

    Update docker_run.sh

--- splatam/docker_run.sh
+++ splatam/docker_run.sh
@@ -2,10 +2,13 @@
 # When <container_name> is not provided default container_name ros2_container is used
 container_name=${1:-splatam_container}
 image_name=${2:-splatam_docker}
-#docker run --rm -it --privileged --runtime=nvidia --gpus all --network=host --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/raviteja/code/:/home/raviteja/code/ --name $container_name $image_name bash
 
-docker run -it --rm --volume="/home/rbccps23/code/splatam/:/SplaTAM/" --env="NVIDIA_VISIBLE_DEVICES=all" --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+# Allow X server connection
+xhost +local:root
+docker rm $container_name
+docker run -it --volume="/media/raviteja/Disk21/datasets:/root/code/datasets" --volume="/media/raviteja/Disk21/code_outputs/splatam:/root/code/output" --env="NVIDIA_VISIBLE_DEVICES=all" --env="NVIDIA_DRIVER_CAPABILITIES=all" \
     --net=host \
+    --ipc=host\
     --privileged \
     --group-add audio \
     --group-add video \
@@ -14,3 +17,6 @@ docker run -it --rm --volume="/home/rbccps23/code/splatam/:/SplaTAM/" --env="NVI
     --gpus all \
     --ipc=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
     --name $container_name $image_name bash
+
+# Disallow X server connection
+xhost -local:root
